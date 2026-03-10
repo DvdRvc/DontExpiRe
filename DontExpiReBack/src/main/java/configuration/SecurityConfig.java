e3f46066df0d7569ca8c40518e8a96100c2a7790
dvdrvc <davidravicc@gmail.com>
2026-03-10 17:29:24 +0100

    reg/log start

---
+++ DontExpiReBack/src/main/java/configuration/SecurityConfig.java
@@ -0,0 +1,27 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .build();
+    }
+
+}
